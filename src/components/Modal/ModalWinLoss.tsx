65f05d5700a56f4a1163c5015b404e507a69e663
James Finn <jamesfinn@Jamess-MacBook-Pro.local>
2024-09-16 02:55:05 -0700

    feat: added copying results to clipboard

--- src/components/Modal/ModalWinLoss.tsx
+++ src/components/Modal/ModalWinLoss.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Modal.module.scss'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from 'src/store'
@@ -8,9 +8,39 @@ import { resetGame, setShowModal } from '@slices/gameSlice'
 import { AppDispatch } from '../../store'
 
 export const ModalWinLoss: React.FC = () => {
-  const { guesses, gameStatus } = useSelector((state: RootState) => state.game)
+  const { guesses, gameStatus, code } = useSelector(
+    (state: RootState) => state.game
+  )
+  const [copyMessage, setCopyMessage] = useState('')
   const dispatch = useDispatch<AppDispatch>()
   const isWin = gameStatus === 'win'
+
+  const copyResultsToClipboard = async () => {
+    try {
+      let copiedResults = 'Code Breaker\n'
+      const codeStr = code.join('')
+      for (let g = 0; g < guesses.length; g++) {
+        copiedResults +=
+          guesses[g]
+            .map((guessNum, i) => {
+              if (guessNum === codeStr[i]) {
+                return '🟢'
+              } else if (codeStr.includes(guessNum)) {
+                return '🟠'
+              } else {
+                return '🔴'
+              }
+            })
+            .join('') + '\n'
+      }
+      copiedResults += 'https://jamesfinn180.github.io/code-breaker/'
+      await navigator.clipboard.writeText(copiedResults)
+      setCopyMessage('Results copied to clipboard')
+    } catch (err) {
+      console.error('Failed to copy text: ', err)
+      setCopyMessage('Failed to copy results!')
+    }
+  }
 
   return (
     <>
@@ -23,10 +53,11 @@ export const ModalWinLoss: React.FC = () => {
 
       <div className={styles.Gap}></div>
 
+      <p>{copyMessage}</p>
       <div className={styles.BtnContainer}>
         <button
           className={clsx(styles.Button, styles.Button_pill)}
-          onClick={() => alert('Not available yet. Coming soon.')}
+          onClick={copyResultsToClipboard}
         >
           Copy Results
         </button>
